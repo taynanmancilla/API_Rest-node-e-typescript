e2300faeef21f929bc18b06e5742e4f6f300027e
taynanmancilla <taynan.estudo@gmail.com>
2023-12-04 13:01:16 -0400

    endpoints_e_statuscode

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import { router } from './routes'; // Importa o objeto 'router' do arquivo de rotas index.js (endpoints)
 
-const server = express();
+const server = express(); // Cria instancia do express pra configurar o servidor
 
-server.get('/', (req, res) => {
-    return res.send('Hello World');
-});
+// adiciona um middleware ao aplicativo Express que analisa corpos de requisições JSON automaticamente e transforma em um objeto JS.
+server.use(express.json());
 
-export { server };
+// adiciona o objeto 'router' como um middleware no aplicativo express tornando as rotas e os endpoints acessiveis pelo servidor
+server.use(router);
+
+export { server }; // Exporta o servidor express configurado
