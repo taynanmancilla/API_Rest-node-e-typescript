6737ad5508d840638aa8275e71666630005456bc
Taynan <taynan.estudo@gmail.com>
2023-12-06 15:43:25 -0400

    build_configs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { server } from './Server/Server';
+import { server } from './server/Server';
 
 server.listen(process.env.PORT || 3333, () => {
     console.log(`App rodando na porta ${process.env.PORT || 3333}!`);
